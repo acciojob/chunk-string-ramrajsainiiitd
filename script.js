c9d5509eb8b78fe71e1501ba9d2ced8d0390124b
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-13 22:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function stringChop(str, size) {
+function stringChop(str, length) {
   // your code here
+	arr = [];
+	if (str.length == 0) {
+		return arr;
+	}
+	else{
+		return str.match(new RegExp('.{1,' + length + '}', 'g'));
+		console.log("wer");
+	}
 }
-
-// Do not change the code below
+// Do not change the code belo
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
